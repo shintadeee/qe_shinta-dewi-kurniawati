ed0954db2a599e9d87e7cd572fae80084f20f995
Shinta Dewi Kurniawati <120574948+shintadeee@users.noreply.github.com>
2023-03-11 21:07:49 +0700

    Add files via upload

---
+++ 6_Basic Programming & OOP/Praktikum/Basic Programming/SegitigaAsterik.java
@@ -0,0 +1,22 @@
+package segitiga.asterik;
+public class SegitigaAsterik{
+    public static void printAsterik(int n){
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for (int k=0; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args){
+        printAsterik(5);
+    }
+}
+
+
+
+
